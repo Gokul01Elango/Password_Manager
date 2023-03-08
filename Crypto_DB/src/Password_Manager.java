4beabf68cfae6972c7e6b43bb5a742eb3cfdae2f
Gokul E <100542178+Gokul01Elango@users.noreply.github.com>
2023-03-08 15:50:43 +0530

    Add files via upload

---
+++ Crypto_DB/src/Password_Manager.java
@@ -0,0 +1,12 @@
+
+
+
+public class Password_Manager {
+
+    public static void main(String[] args) {
+
+
+        Master_Login ml=new Master_Login();
+        ml.setVisible(true);
+    }
+}
---
+++ DB_Con.sql
@@ -0,0 +1,9 @@
+-- Signup Table
+create table signup(site_name VARCHAR(50) NOT NULL,user_name varchar(30) NOT NULL, password varchar(50) NOT NULL);
+select * from signup;
+drop table signup;
+
+-- New Data Insertion 
+create table adddata(site_name VARCHAR(50) NOT NULL,user_name varchar(30) NOT NULL, password varchar(50) NOT NULL);
+select * from adddata;
+drop table adddata;
